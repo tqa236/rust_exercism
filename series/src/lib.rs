0549c29f8652ac11bbaad593a86bac1c681558a6
Trinh Quoc Anh <trinhquocanh94@gmail.com>
2024-12-15 18:00:30 +0100

    Solve exercise

---
+++ series/src/lib.rs
@@ -0,0 +1,12 @@
+pub fn series(digits: &str, len: usize) -> Vec<String> {
+    if len == 0 || digits.len() < len {
+        return vec![];
+    }
+
+    let mut result = Vec::new();
+    for i in 0..=digits.len() - len {
+        result.push(digits[i..i + len].to_string());
+    }
+
+    result
+}
